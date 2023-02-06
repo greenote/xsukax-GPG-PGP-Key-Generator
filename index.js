0902c93fa2cd471c6ade2b89b62f48a1142d8c99
FELIX <felixadegboyega2019@gmail.com>
2023-02-06 10:14:28 +0100

    configured sequelize

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express  = require('express')
+const express = require('express')
 const app = express()
-const port = process.env.PORT | 300;
+const port = process.env.PORT | 3000;
 const conn = require('./config/db')
-const routes = require('./router/index') 
+const routes = require('./router/index')
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 //app.use(cors({origin:'*'}))
 
-conn.connect((err)=>{
+conn.connect((err) => {
     err ? console.log(err) : console.log("database is connected")
 })
 
 app.use('/api', routes)
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("serve start runing " + port)
 })
