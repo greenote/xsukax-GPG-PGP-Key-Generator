c8c098a984b3064fe9b3569aec780fe1ea738d17
FELIX <felixadegboyega2019@gmail.com>
2023-02-07 15:59:41 +0100

    created user and chat model

---
+++ migrations/20230207135227-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      token: {
+        type: Sequelize.STRING(50),
+      },
+      status: {
+        type: Sequelize.STRING(50),
+      },
+      expire_time: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ migrations/20230207142154-create-chat.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Chats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      message: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      senderId: {
+        type: Sequelize.INTEGER,
+        references: {model: 'Users', key: 'id'},
+        onDelete: 'NO ACTION',
+        onUpdate: 'NO ACTION',
+      },
+      receiverId: {
+        type: Sequelize.INTEGER,
+        references: {model: 'Users', key: 'id'},
+        onDelete: 'NO ACTION',
+        onUpdate: 'NO ACTION',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Chats');
+  }
+};
