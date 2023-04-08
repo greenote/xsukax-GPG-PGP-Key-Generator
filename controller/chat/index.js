6735a744ac08b1496e179d9ea1a65e60250446c4
Felixadegboyega <yofelixadegboyega2019@gmail.com>
2023-04-08 10:32:02 +0100

    added socket events for `user typing`

--- controller/chat/index.js
+++ controller/chat/index.js
@@ -10,7 +10,7 @@ const socket = (io) => {
 
 			// if user found update socketId
 			if (findUser) {
-				let index = allUsers.indexOf(findUser)
+				const index = allUsers.indexOf(findUser)
 				let user = {
 					userId,
 					socketId: socket.id
@@ -19,42 +19,34 @@ const socket = (io) => {
 				return
 			}
 			// if user not found, add to user array
-			let user = {userId, socketId: socket.id}
+			const user = {userId, socketId: socket.id}
 			allUsers.push(user)
 			socket.emit('GET-ACTIVE-USERS', allUsers)
 		});
 
 		socket.on('SEND-MESSAGE', (data) => {
 			// get the receiver socket id
-			let receiver = allUsers.find(each => (each.userId == data.receiverId));
+			const receiver = allUsers.find(each => (each.userId == data.receiverId));
 			if (receiver) {
 				socket.to(receiver.socketId).emit('RECEIVE-MESSAGE', data);
 			}
 		})
 
-		socket.on('disconnect', function () {
-			let findUser = allUsers.find(each => each.socketId == socket.id)
-			if (findUser) {
-				let index = allUsers.indexOf(findUser)
-				allUsers.splice(index, 1);
+		// Typing socket method #### This is capable of firing a message to the receiver
+		socket.on('I-AM-TYPING', (data) => {
+			const receiver = allUsers.find(each => (each.userId == data.receiverId));
+			if (receiver) {
+				socket.to(receiver.socketId).emit('USER-IS-TYPING', data);
 			}
 		})
 
-		//Typing socket method #### This is capable of firing a message to the receiver
-		// socket.on('typing', (res) => {
-		// 	console.log('for typing', res);
-		// 	allUsers.find(e => {
-		// 		//  console.log('res.user', res)
-		// 		if (e.receiver === res.name) {
-		// 			socket.broadcast.to(e.socket_id).emit('user_typing', {...res, typing: 'typing....'})
-		// 		} else {
-		// 			console.log('undefined');
-		// 		}
-		// 	})
-		// })
-
-		// socket.emit("welcome", "i see user here")
-
+		socket.on('disconnect', function () {
+			const findUser = allUsers.find(each => each.socketId == socket.id)
+			if (findUser) {
+				const index = allUsers.indexOf(findUser)
+				allUsers.splice(index, 1);
+			}
+		})
 	});
 
 }
