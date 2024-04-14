1075f7932fb1ff6da0e2205cf6eca6eab37ac9fb
Licode <felixadegboyega2019@gmail.com>
2024-04-14 14:28:56 +0100

    fix: smsResponse format on "/register"

--- utilities/bulksms.js
+++ utilities/bulksms.js
@@ -1,51 +1,52 @@
-require('dotenv').config()
-const axios = require('axios')
-// const express = require('express');
-const {EBULKSMS_USERNAME, EBULKSMS_APIKEY} = process.env
+require("dotenv").config()
+const axios = require("axios")
+const { EBULKSMS_USERNAME, EBULKSMS_APIKEY } = process.env
 
 async function _sms(data) {
-    try {
-        const config = {
-            method: "post",
-            url: "https://api.ebulksms.com:8080/sendsms.json",
-            data: {
-                "SMS": {
-                    "auth": {
-                        "username": `${EBULKSMS_USERNAME}`,
-                        "apikey": `${EBULKSMS_APIKEY}`
-                    },
-                    "message": {
-                        "sender": "Green NOTE",
-                        "messagetext": `Welcome ${data.token} is your Greenote verification code.`,
-                        "flash": "0"
-                    },
-                    "recipients": {
-                        "gsm":
-                        {
-                            "msidn": `${data.phone}`,
-                            "msgid": "Otp"
-                        },
-
-                    },
-                    "dndsender": 1
-                }
-            }
-        }
-        const bulkRes = await axios(config)
-        let _smsRes = bulkRes.data
-        if (_smsRes.response.status == "SUCCESS") {
-            let message = "Your One time password(OTP) has been sent to you";
-            return {message, success: true}
-        } else {
-            let message = "Can't send OTP";
-            return {message, success: false};
-        }
-
-    } catch (error) {
-        let message = error
-        return {message, success: false};
+  try {
+    const config = {
+      method: "post",
+      url: "https://api.ebulksms.com:8080/sendsms.json",
+      data: {
+        SMS: {
+          auth: {
+            username: `${EBULKSMS_USERNAME}`,
+            apikey: `${EBULKSMS_APIKEY}`,
+          },
+          message: {
+            sender: "Greenote",
+            messagetext: `Welcome ${data.token} is your Greenote verification code.`,
+            flash: "0",
+          },
+          recipients: {
+            gsm: {
+              msidn: `${data.phone}`,
+              msgid: "Otp",
+            },
+          },
+          dndsender: 1,
+        },
+      },
     }
-
+    const bulkRes = await axios(config)
+    let _smsRes = bulkRes.data
+    if (_smsRes.response.status == "SUCCESS") {
+      return {
+        message: "Your One time password(OTP) has been sent to you",
+        success: true,
+      }
+    } else {
+      return {
+        message: "Couldn't send OTP",
+        success: false,
+      }
+    }
+  } catch (error) {
+    return {
+      message: error?.message ? error.message : "Couldn't send OTP",
+      success: false,
+    }
+  }
 }
 
-module.exports = {_sms}
+module.exports = { _sms }
